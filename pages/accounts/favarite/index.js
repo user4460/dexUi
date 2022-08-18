fe5e4ad824410647537bfee20dda6d4d2ad62fbe
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-08-18 13:49:15 +0800

    setting

--- pages/accounts/favarite/index.js
+++
@@ -1,8 +0,0 @@
-export default function favarite() {
-   return (
-      <>
-         <h1>favarite</h1>
-         <h2>test</h2>
-      </>
-   )
-}
--- pages/accounts/setting/index.js
+++ pages/accounts/setting/index.js
@@ -1,8 +1,27 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
 export default function Mypage() {
    return (
       <>
-         <h1>MyPage</h1>
-         <h2>test</h2>
+         <Tabs>
+            <TabList>
+               <Tab>One</Tab>
+               <Tab>Two</Tab>
+               <Tab>Three</Tab>
+            </TabList>
+
+            <TabPanels>
+               <TabPanel>
+                  <p>one!</p>
+               </TabPanel>
+               <TabPanel>
+                  <p>two!</p>
+               </TabPanel>
+               <TabPanel>
+                  <p>three!</p>
+               </TabPanel>
+            </TabPanels>
+         </Tabs>
       </>
    )
 }
