fee51657225bbfe09d627e2088003a8d34af823b
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-09-16 14:34:42 +0800

    stack

--- pages/create/index.js
+++ pages/create/index.js
@@ -1,7 +1,7 @@
 //取引を作成する、ページ、広告なども、
 //掲示板機能のオファーみたいなもの
 //loginした人だけのバリデートを入れる
-//
+//技術スタックも記載する
 
 import { Checkbox, Stack, Input, Select, Textarea, Text } from "@chakra-ui/react"
 import { useState } from "react"
